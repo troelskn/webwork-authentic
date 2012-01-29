669a68f6526ad010a9eb802844db348d01cdb080
Troels Knak-Nielsen <troelskn@gmail.com>
2012-01-29 16:40:08 +0100

    Use the stricter method_exists than is_callable

--- lib/authentic_globals.inc.php
+++ lib/authentic_globals.inc.php
@@ -12,7 +12,7 @@ function authorise_user_by_credentials() {
     $GLOBALS['current_user'] = $user;
     if (in_array('last_logged_in_at', authentic_users()->getColumnNames())) {
       $user->lastLoggedInAt = date("Y-m-d H:i:s");
-      if (is_callable(array($user, 'onlyValidateCredentialsOnSave'))) {
+      if (method_exists($user, 'onlyValidateCredentialsOnSave')) {
         $user->onlyValidateCredentialsOnSave();
       }
       authentic_users()->saveOrFail($user);
